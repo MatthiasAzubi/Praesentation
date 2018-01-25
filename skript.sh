cd84559488a0f770017b693109005c2ba8fe5d56
Matthias <matthias@pc-indy67.se.informatik.tu-darmstadt.de>
2018-01-25 07:36:47 +0100

    Ende hinzugefügt

--- skript.sh
+++ skript.sh
@@ -3,3 +3,6 @@
 echo 'Hallo zusammen!'
 echo 'Ich freue mich, dass Sie hier sind!'
 echo 'Ich möchte Ihnen heute etwas über das Versionskontrollsystem Git erzählen.'
+
+echo '...'
+echo 'Ich bedanke mich für Ihre Aufmerksamkeit!'
